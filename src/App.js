c191bf5e673715fe004a9652c00b7f6bf21b3140
Philippe Offermans <philippeoffermans@gmail.com>
2022-04-07 22:52:13 +0200

    fixing the repo, added library

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+//Import Styles
+import "./styles/app.scss";
+//Adding Components
+import Player from "./components/Player";
+import Song from "./components/Song";
+import Library from "./components/Library"
+//Import Util
+import data from "./util";
+
+
+function App() {
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  return (
+    <div className="App">
+      <Song currentSong={currentSong} />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
+      <Library songs={songs}/>
+    </div>
+  );
+}
+
+export default App;
