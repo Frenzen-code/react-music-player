aa29f0cfd42de24c7870d0caa23fe32d1f0b6f51
Philippe Offermans <philippeoffermans@gmail.com>
2022-04-08 16:56:50 +0200

    adding a toggle feature to the library

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 //Import Styles
 import "./styles/app.scss";
 //Adding Components
 import Player from "./components/Player";
 import Song from "./components/Song";
-import Library from "./components/Library"
+import Library from "./components/Library";
+import Nav from "./components/Nav";
 //Import Util
 import data from "./util";
 
-
 function App() {
+  // Ref
+  const audioRef = useRef(null);
   //State
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+  });
+  const [libraryStatus, setLibraryStatus] = useState(false);
+  // Event handler
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({ ...songInfo, currentTime: current, duration });
+  };
 
   return (
     <div className="App">
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
       <Song currentSong={currentSong} />
       <Player
+        audioRef={audioRef}
         isPlaying={isPlaying}
         setIsPlaying={setIsPlaying}
         currentSong={currentSong}
+        setSongInfo={setSongInfo}
+        songInfo={songInfo}
       />
-      <Library songs={songs}/>
+      <Library
+        audioRef={audioRef}
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        isPlaying={isPlaying}
+        setSongs={setSongs}
+      />
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+      ></audio>
     </div>
   );
 }
